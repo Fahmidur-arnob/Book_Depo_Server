ff667be07a2553b0271e64ae1fb107a490115071
Fahmidur-arnob <arnobdev93@gmail.com>
2022-11-30 20:15:20 +0600

    Made some changes in vercel file

--- index.js
+++ index.js
@@ -131,19 +131,6 @@ async function run () {
             res.send({ isAdmin: user?.role === 'admin' });
         })
 
-        app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) }
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    role: 'admin'
-                }
-            }
-            const result = await usersCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        });
-
     }
     finally{}
 }
